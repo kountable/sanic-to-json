9fb921de5ebb0940a179bac3c2ec7e785178957c
caheredia <cheredia@kountable.com>
2019-07-03 17:25:03 -0700

    format api JSON

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # python_to_postman
 Generate Postman JSON file from python endpoints
 
-Using the postman [schema](https://schema.getpostman.com/json/collection/v2.1.0/collection.json) we can build Postman Collections using python endpoints from Sanic or Flask. Note: endpoints need to be designated with `@`, for example `@app.route("example-route")`. 
+Using the postman [schema](https://schema.getpostman.com/json/collection/v2.1.0/collection.json) we can build Postman Collections using python endpoints from Sanic or Flask. The script reads Sanic app. It searches for blueprints. The blueprints, through routes, provide docs strings data. The doc string data is used to populate a Postman formatted JSON file. The JSON file can be uploaded to Postman as a collection. 
 
--- read_routes.py
+++ read_routes.py
@@ -1,9 +1,31 @@
-from routes import acp_v1
+from app import app
 
-# print bluepint metadata version
-print(acp_v1.name, acp_v1.version, acp_v1.__doc__)
 
-# print endpoints with doc strings
-for route in acp_v1.__dict__["routes"]:
-    print(route[1])
-    print(route[0].__doc__)
+atomic_endpoint = {
+    "name": "test request",
+    "request": {
+        "method": "GET",
+        "header": [],
+        "url": {
+            "raw": "{{target_url}}test-employees",
+            "host": ["{{target_url}}test-employees"],
+        },
+        "description": "This description can come from docs strings",
+    },
+    "response": [],
+}
+
+
+def format_endpoint(route, postman_request=atomic_endpoint):
+    """Populates basic_endpoint dictionary with route metatdata.
+    Returns a formatted postman request item."""
+
+    name = route[1]
+    description = route[0].__doc__
+    url = "{{target_url}}" + name[1:]
+    postman_request["name"] = name
+    postman_request["request"]["url"]["raw"] = url
+    postman_request["request"]["url"]["host"] = [url]
+    postman_request["request"]["description"] = description
+    return postman_request
+
