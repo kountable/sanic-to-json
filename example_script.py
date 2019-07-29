54b43aed718598433fc5da56dedb32f01b17ccd2
caheredia <cheredia@kountable.com>
2019-07-29 16:49:46 -0700

    rename files

---
+++ example_script.py
@@ -0,0 +1,5 @@
+from python_to_postman import generate_postman_json
+from app import app
+
+
+generate_postman_json("Test_collection", app)
--- python_to_postman.py
+++ python_to_postman.py
@@ -1,5 +1,94 @@
-from read_routes import generate_postman_json
-from app import app
+from json import dump
+
+collection_json = {
+    "info": {
+        "name": "Test_collection",
+        "description": "Generic text used for documentation.",
+        "schema": "https://schema.getpostman.com/json/collection/v2.1.0/collection.json",
+    },
+    "item": [],
+}
+
+atomic_request = {
+    "name": "test request",
+    "request": {
+        "method": "GET",
+        "header": [],
+        "url": {"raw": "{{target_url}}endpoint", "host": ["{{target_url}}endpoint"]},
+        "description": "This description can come from docs strings",
+    },
+    "response": [],
+}
 
 
-generate_postman_json("Test_collection", app)
+def postman_JSON(collection_name, app, api_json=collection_json):
+    """Formats the Postman collection with 'collection_name' and doc string from Sanic app.
+    
+    Returns JSON dictionary."""
+
+    api_json["info"]["name"] = collection_name
+    api_json["info"]["description"] = app.__doc__
+    return api_json
+
+
+def blueprint_items(api_json, app):
+    """Converts each blueprint into a dictionary with a name, item =[], and description.
+
+    These dictionaries become Postman folders. The "item" dict will contain the endpoins
+    for each blueprint.
+
+    Returns a list of dictionary items."""
+
+    blueprints = list(app.__dict__["blueprints"].keys())
+    for blueprint in blueprints:
+        postman_folder = {}
+        postman_folder["name"] = blueprint
+        postman_folder["item"] = []
+        postman_folder["description"] = app.blueprints[blueprint].__doc__
+        api_json["item"].append(postman_folder)
+    return api_json
+
+
+def populate_requests(api_json, app):
+    """Populates the requests for each blueprint.
+    
+    Returns the items list with 'item' key popluated with requests."""
+    for blueprint_dict in api_json["item"]:
+        for route in app.blueprints[blueprint_dict["name"]].routes:
+            blueprint_dict["item"].append(format_endpoint(route))
+    return api_json
+
+
+def format_endpoint(route, postman_request=atomic_request.copy()):
+    """Populates atomic_request dictionary with route metatdata.
+
+    Assumes route is a list of route items, e.g, function, url, methods
+
+    Returns a postman formatted dictionary request item."""
+
+    name = route[1]
+    description = route[0].__doc__
+    url = "{{target_url}}" + name[1:]
+    postman_request = {}
+    postman_request["name"] = name
+    postman_request["request"] = {}
+    postman_request["request"]["url"] = {}
+    postman_request["request"]["url"]["raw"] = url
+    postman_request["request"]["url"]["host"] = [url]
+    postman_request["request"]["description"] = description
+    return postman_request
+
+
+def generate_postman_json(collection_name, app, filename="postman_collection.json"):
+    """Write a JSON file to Postman schema specifications."""
+    # new json dictionary
+    api_collection = postman_JSON(collection_name, app=app)
+
+    # generate a list of items
+    api_collection = blueprint_items(api_collection, app)
+
+    # poulate blueprints with endpoints
+    api_collection = populate_requests(api_collection, app)
+
+    with open(filename, "w") as file:
+        dump(api_collection, file, indent=4)
