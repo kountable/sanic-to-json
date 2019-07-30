a5f6dbcbe15af8db0862960168f5235b8a6bf0ab
caheredia <cheredia@kountable.com>
2019-07-30 14:11:28 -0700

    add enpoint to app

--- Examples/app.py
+++ Examples/app.py
@@ -10,15 +10,18 @@ app.blueprint([bp1, bp2])
 
 @app.listener("before_server_start")
 async def setup_db(app, loop):
-    logger.debug("root before_server_start 'opening thrift transport'")
-    transport.open()
+    pass
 
 
 @app.listener("after_server_stop")
 async def close_db(app, loop):
-    logger.debug("root after_server_start 'closing thrift transport'")
-    transport.close()
+    pass
+
+
+@app.route("/healthcheck", methods=["GET"])
+async def post(request):
+    """Return health status."""
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=local_instance)
+    app.run(host="0.0.0.0")
