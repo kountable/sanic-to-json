7923b5bd2b5c7631ad6ad556a951f2e4130acb9c
caheredia <cheredia@kountable.com>
2019-07-31 16:08:18 -0700

    convert dicionaries to functions

--- sanic_to_json.py
+++ sanic_to_json.py
@@ -1,32 +1,43 @@
 from json import load
 from examples.app import app
 
-collection_json = {
-    "info": {
-        "name": "Test_collection",
-        "description": "Generic text used for documentation.",
-        "schema": "https://schema.getpostman.com/json/collection/v2.1.0/collection.json",
-    },
-    "item": [],
-}
 
-atomic_request = {
-    "name": "test request",
-    "request": {
-        "method": "GET",
-        "header": [],
-        "url": {"raw": "{{target_url}}endpoint", "host": ["{{target_url}}endpoint"]},
-        "description": "This description can come from docs strings",
-    },
-    "response": [],
-}
+def collection_json():
+    """Returns JSON skeleton for Postman schema."""
+    collection_json = {
+        "info": {
+            "name": "Test_collection",
+            "description": "Generic text used for documentation.",
+            "schema": "https://schema.getpostman.com/json/collection/v2.1.0/collection.json",
+        },
+        "item": [],
+    }
+    return collection_json
 
 
-def basic_JSON(collection_name, app, api_json=collection_json):
+def atomic_request():
+    """Returns an atomic Postman request dictionary."""
+
+    request = {
+        "name": "test request",
+        "request": {
+            "method": "GET",
+            "header": [],
+            "url": {
+                "raw": "{{target_url}}endpoint",
+                "host": ["{{target_url}}endpoint"],
+            },
+            "description": "This description can come from docs strings",
+        },
+        "response": [],
+    }
+    return request
+
+
+def basic_JSON(collection_name, app, api_json=collection_json()):
     """Formats the Postman collection with 'collection_name' and doc string from Sanic app.
     
     Returns JSON dictionary."""
-
     api_json["info"]["name"] = collection_name
     api_json["info"]["description"] = app.__doc__
     return api_json
@@ -64,8 +75,23 @@ def get_blueprint_routes(blueprint, app):
 
 def get_route_name(route):
     """Returns route name."""
-    name = route[0].split("/")[-1]
+    name = route[1]
     return name
+
+
+def get_doc_string(route):
+    """Return doc string for function in route."""
+    return route[0].__doc__
+
+
+def get_route_method(route):
+    """Return route CRUD method."""
+    return route[2][0]
+
+
+def get_url_prefix(blueprint):
+    prefix = app.blueprints[blueprint].version + app.blueprints[blueprint].url_prefix
+    return prefix
 
 
 collection = basic_JSON("Testing", app)
@@ -74,8 +100,14 @@ collection = transfer_postman_id(collection)
 
 # blueprints = get_blueprint_docs("database_1", app)
 blueprints = find_blueprints(app)
-print(blueprints)
-routes = get_blueprint_routes("api_1", app)
-# test = get_route_name(routes[0])
+for blueprint in blueprints:
+    print(get_url_prefix(blueprint))
+    routes = get_blueprint_routes(blueprint, app)
+    for route in routes:
+        print(get_route_name(route))
+        print(get_route_method(route))
+        print(get_doc_string(route))
+
+
 # test = routes[0][1][0].handlers["GET"].__doc__
-print(routes[0][0].__dir__())
+
