dbe4ef02bd2d1d8f2436e8c91b6fd4a78859218a
caheredia <cheredia@kountable.com>
2019-07-31 14:20:25 -0700

    add get_route_name function

--- sanic_to_json.py
+++ sanic_to_json.py
@@ -1,4 +1,5 @@
 from json import load
+from examples.app import app
 
 collection_json = {
     "info": {
@@ -21,24 +22,39 @@ atomic_request = {
 }
 
 
-def transfer_postman_id(api_json, existing_file=None):
-    """Transfer postman_id from existing JSON file."""
-    try:
-        with open(existing_file, "r") as file:
-            data = load(file)
-            api_json["info"]["_postman_id"] = data["info"]["_postman_id"]
-    except TypeError:
-        pass
-
-    return api_json
+def find_blueprints(app):
+    """Returns a list of blueprints."""
+    blueprints = app.blueprints.keys()
+    return list(blueprints)
 
 
-def basic_JSON(collection_name, app, api_json=collection_json):
-    """Returns skeleton JSON for Postman collection file.
-    
-    Collects 'collection_name' from argument and doc string from Sanic app.
-    Returns JSON dictionary."""
+def get_blueprint_docs(blueprint, app):
+    """Returns doc string for blueprint."""
+    doc_string = app.blueprints[blueprint].__doc__
+    return doc_string
 
-    api_json["info"]["name"] = collection_name
-    api_json["info"]["description"] = app.__doc__
-    return api_json
+
+def get_blueprint_routes(blueprints, app):
+    """Return a list of routes."""
+    routes = []
+    for blueprint in blueprints:
+        for route_name in app.router.routes_names:
+            if blueprint in route_name:
+                routes.append(app.router.routes_names[route_name])
+    return routes
+
+
+def get_route_name(route):
+    """Returns route name."""
+
+
+collection = basic_JSON("Testing", app)
+collection = transfer_postman_id(collection)
+
+
+# blueprints = get_blueprint_docs("database_1", app)
+blueprints = find_blueprints(app)
+routes = get_blueprint_routes(blueprints, app)
+test = routes[1]
+# test = routes[0][1][0].handlers["GET"].__doc__
+print(test)
