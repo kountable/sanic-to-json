da810a844aaae9a4c301a27e328bdf3c0e1b9356
caheredia <cheredia@kountable.com>
2019-08-30 16:25:18 -0700

    clean up blueprints

--- examples/blueprint_2.py
+++ examples/blueprint_2.py
@@ -5,13 +5,6 @@ bp2 = Blueprint("api_2", url_prefix="/another-prefix", version="v1")
 bp2.__doc__ = "This is the doc string for blueprint 2."
 
 
-@bp2.route("/endpoint-three", methods=["GET"])
-async def post(request):
-    """Return text from request."""
-    data = request.text
-    return text(data)
-
-
 @bp2.route("/endpoint-four", methods=["GET"])
 async def post(request):
     """Return JSON form request."""
